b409d0c74b0590701920d15a214d2c578229eee1
Kevin Whinnery <kevin.whinnery@gmail.com>
2023-06-21 09:45:43 +0200

    add demo, fix bug

--- demos/fresh_kv_api/routes/kv/[...path].ts
+++ demos/fresh_kv_api/routes/kv/[...path].ts
@@ -2,5 +2,5 @@ import { Handlers } from "$fresh/server.ts";
 import generateHandlers from "../../../../fresh.ts";
 
 export const handler: Handlers = generateHandlers({
-  prefix: "kv",
+  prefix: "/kv",
 });
